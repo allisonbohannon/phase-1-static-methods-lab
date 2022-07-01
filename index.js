db18b3040e8f40cf647c6d6f48d69f6bd3ce557e
Allison Bohannon <akboh.github@gmail.com>
2022-07-01 10:27:35 -0700

    commit tree a0196d8e7828d6f627b7a86880bd7bbe0a4c9334

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    const arr = string.split('');
+    arr[0] = arr[0].toUpperCase()
+    const newString = arr.join(''); 
+    return newString; 
+  }
+
+  static sanitize(string) {
+    const arr = string.split('');
+    const regEx =  /^[a-z0-9]+$/i
+    const newArr = arr.filter(value => value = regEx || value === '-' || value === `'` || value === ' ')
+    const newString = newArr.join('')
+  }
+
+  static titleize(string) { 
+    const arr = string.split('');
+    arr[0] = arr[0].toUpperCase()
+
+  }
 }
+
+Formatter.capitalize('alligator')
